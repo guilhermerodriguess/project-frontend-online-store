7e07f430cbd5d13727befc2b0546dafa6598460a
Rafael-Friedel <RafaFriedelDev@outlook.com>
2022-03-10 17:05:40 -0300

    REQUISITO 1 - IMPLEMENTO FETCH

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,14 @@
 export async function getCategories() {
-  // Implemente aqui
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const promisse = fetch(endpoint)
+    .then((pro) => pro.json())
+    .then((data) => data);
+  return promisse;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (!query && !categoryId) throw new Error('You must provide an url');
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const data = await response.json();
+  return data;
 }
