0629649153b14df95bf30501b0a6f3422c86d2cd
Rafael-Friedel <RafaFriedelDev@outlook.com>
2022-03-11 16:06:28 -0300

    group 28 - REQUISITO 4

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  async componentDidMount() {
+    const categories = await getCategories();
+    this.setState({ categories });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <>
+        {categories.map((categori) => (
+          <label htmlFor={ categori.id } data-testid="category" key={ categori.id }>
+            <input
+              type="radio"
+              id={ categori.id }
+              name={ categori.id }
+              value={ categori.name }
+            />
+            {categori.name}
+          </label>
+        ))}
         <input />
         <span data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
