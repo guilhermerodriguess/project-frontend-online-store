307637bdc96b3e9cbf168b12887285bea47d4a8b
Rafael-Friedel <RafaFriedelDev@outlook.com>
2022-03-15 12:15:43 -0300

    adicionando link no componente card

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class Card extends React.Component {
   render() {
-    const { title, price, thumbnail } = this.props;
+    const { id, title, price, thumbnail } = this.props;
     return (
       <div
         data-testid="product"
@@ -12,6 +13,10 @@ class Card extends React.Component {
         <p data-testid="shopping-cart-product-name">{ title }</p>
         <img src={ thumbnail } alt={ title } />
         <p data-testid="shopping-cart-product-quantity">1</p>
+        <Link
+          data-testid="product-detail-link"
+          to={ `/product-detail/${id}` }
+        />
         <p>{ price }</p>
       </div>
     );
@@ -22,6 +27,7 @@ Card.propTypes = {
   title: PropTypes.string.isRequired,
   thumbnail: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Card;
